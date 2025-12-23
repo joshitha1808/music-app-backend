b31e6723ec9c8d76de869ba4dd34325282ca57f5
joshitha0818 <joshitha0818@gmail.com>
2025-12-24 00:15:12 +0530

    Refactor : database setup and user model definition

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from sqlalchemy import create_engine
+from sqlalchemy import TEXT, VARCHAR, Column, create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 app=FastAPI()
 
 DATABASE_URL='postgresql://postgres:g4st=sTevi@localhhost:5432/musicapp'
 
 engine=create_engine(DATABASE_URL)
-sessionlocal=sessionmaker()
+sessionlocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+db=sessionlocal()
 
 class UserCreate(BaseModel):
     name:str
     email:str
     password:str
+Base=declarative_base()
+
+class User(Base):
+    __tablename__='users'
+
+    id=Column(TEXT)
+    Name=Column(VARCHAR(100))
+    email=Column(VARCHAR(100))
+    password=Column(TEXT)
+
 
 
 @app.post('/signup')
