eba4a33b0db0a24745c74f89e5ff7e9952f3aff4
joshitha1808 <joshitha0818@gmail.com>
2025-12-29 15:56:48 +0530

    refactor: move jwt auth logic to auth_middleware.py

---
+++ middleware/auth_middleware.py
@@ -0,0 +1,23 @@
+from fastapi import HTTPException, Header
+
+
+def auth_middleware(x_auth_token=Header()):
+    try:
+        # 1. Check for token 
+        if not x_auth_token:
+            raise HTTPException(401, 'No auth token, access denied!')
+        # 2. Decode the token (Logic must be inside 'try')
+        verified_token = jwt.decode(x_auth_token, 'password_key', ['HS256'])
+
+        if not verified_token:
+            raise HTTPException(401, 'Token verification failed, authorization')
+
+        # 3. Get the id from the token
+        uid = verified_token.get('id')
+        return uid
+    
+    #postgress database get the user info 
+    except jwt.PyJWTError:
+        # This catches errors like expired or fake tokens
+        raise HTTPException(401, 'Token is not valid, authorization failed')
+
