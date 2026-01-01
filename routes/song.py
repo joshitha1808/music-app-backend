b3a38ceefb38e3dd6f30ec95627cc7f0061cc8d6
joshitha1808 <joshitha0818@gmail.com>
2026-01-02 00:18:21 +0530

    feat: implement upload song route

---
+++ routes/song.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, Depends,UploadFile,File,Form
+from sqlalchemy.orm import Session
+from database import get_db
+from middleware import auth_middleware
+
+router=APIRouter()
+
+@router.post('/upload')
+def upload_song(
+    song:UploadFile=File(...),
+    thumbnail:UploadFile=File(...),
+    artist:str=Form(...),
+    song_name:str=Form(...),
+    hex_code:str=Form(...),
+    db:Session=Depends(get_db),
+    auth_dict=Depends(auth_middleware),
+):
+    pass
+
+
+
+
