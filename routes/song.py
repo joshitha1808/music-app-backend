88be3d1e506d82bc6f820cfacc17b5a6a999975e
joshitha1808 <joshitha0818@gmail.com>
2026-01-02 16:15:10 +0530

    feat: configure cloudinary for song uploads

--- routes/song.py
+++ routes/song.py
@@ -2,8 +2,21 @@ from fastapi import APIRouter, Depends,UploadFile,File,Form
 from sqlalchemy.orm import Session
 from database import get_db
 from middleware import auth_middleware
+import cloudinary
+import cloudinary.uploader
+from dotenv import load_dotenv
+import os
 
 router=APIRouter()
+
+load_dotenv()
+
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+    secure=True
+)
 
 @router.post('/upload')
 def upload_song(song:UploadFile=File(...),
