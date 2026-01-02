f57463f6e8086465e9213ea4e0debe8f9b42be23
joshitha1808 <joshitha0818@gmail.com>
2026-01-02 15:15:13 +0530

    refactor: streamline upload_song function parameters

--- routes/song.py
+++ routes/song.py
@@ -6,15 +6,13 @@ from middleware import auth_middleware
 router=APIRouter()
 
 @router.post('/upload')
-def upload_song(
-    song:UploadFile=File(...),
-    thumbnail:UploadFile=File(...),
-    artist:str=Form(...),
-    song_name:str=Form(...),
-    hex_code:str=Form(...),
-    db:Session=Depends(get_db),
-    auth_dict=Depends(auth_middleware),
-):
+def upload_song(song:UploadFile=File(...),
+                thumbnail:UploadFile=File(...),
+                artist:str=Form(...),
+                song_name:str=Form(...),
+                hex_code:str=Form(...),
+                db:Session=Depends(get_db),
+                auth_dict=Depends(auth_middleware)):
     pass
 
 
