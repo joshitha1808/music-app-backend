787d80c89fd1105581342694324ef564d98dc97b
joshitha1808 <joshitha0818@gmail.com>
2026-01-14 23:09:13 +0530

    feat: fetch all songs from database

--- routes/song.py
+++ routes/song.py
@@ -61,6 +61,9 @@ def upload_song(
 
 @router.get('/list')
 def list_songs(db: Session=Depends(get_db),auth_details=Depends(auth_middleware)):
+    songs=db.query(Song).all()
+    return songs
+
 
    
 
