0e825cb85e84be7706edfdcff8bb0edbab4a6f12
joshitha1808 <joshitha0818@gmail.com>
2026-01-21 11:12:38 +0530

    feat: Add Song relationship to Favorite model

--- models/favorite.py
+++ models/favorite.py
@@ -1,10 +1,12 @@
 from sqlalchemy import TEXT, Column, ForeignKey
 from models.base import Base
-
+from sqlalchemy.orm import relationship
 
 class Favorite(Base):
-    __tablename__="favorites"
+    __tablename__ = "favorites"
+    id = Column(TEXT, primary_key=True)
+    song_id = Column(TEXT, ForeignKey("songs.id"))
+    user_id = Column(TEXT, ForeignKey("users.id"))
 
-    id=Column(TEXT,primary_key=True) 
-    song_id=Column(TEXT,ForeignKey("songs.id"))
-    user_id=Column(TEXT,ForeignKey("users.id"))
+    song=relationship('Song') 
+ 
