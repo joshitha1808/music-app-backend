18d9d8a9858f72782d89a3bf554630b705f4a227
joshitha1808 <joshitha0818@gmail.com>
2026-01-21 23:56:50 +0530

    feat: Add joinedload import

--- routes/auth.py
+++ routes/auth.py
@@ -10,6 +10,7 @@ from models.user import User
 from pydantic_schemas.user_create import UserCreate
 from sqlalchemy.orm import Session
 from pydantic_schemas.user_login import UserLogin
+from sqlalchemy.orm import joinedload
 
 
 
@@ -59,7 +60,7 @@ def login_user(user:UserLogin,db:Session=Depends(get_db)):
     #if doesnot match return error
 @router.get('/')
 def current_user_data(db: Session = Depends(get_db), user_dict=Depends(auth_middleware)):
-    user=db.query(User).filter(User.id == user_dict['uid']).first()
+    user=db.query(User).filter(User.id == user_dict['uid']).options(joinedload(User.favotites)).first()
 
     if not user:
         raise HTTPException(404,'User not found!')
